58c5cf54fedba717adf1f3df15f64ce3dc06942c
Thord Setsaas <thord@ieee.org>
2015-03-14 15:15:05 +0100

    Added java files for importing sms.db into elasticsearch and updated Readme

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # elasticsmsdb
-Get iOS sms.db into Elasticsearch
+Simple way to load messages from a iOS sms.db into Elasticsearch, and browse the data with Kibana
+
+## Setup
+
+
+### Prerequisites
+#### Installing Elasticsearch
+1. Head on over to the [Elasticsearch Download page](https://www.elastic.co/downloads/elasticsearch) and grab the newest release
+2. Extract it to a folder and execute `$ES_HOME/bin/elasticsearch` (or `$ES_HOME\bin\elasticsearch.bat` on Windows)
+
+#### Installing River JDBC Plugin
+The River JDBC plugin does not come by default with Elasticsearch, and must be installed separately
+
+1. Execute the following command: `./bin/plugin --install jdbc --url http://xbib.org/repository/org/xbib/elasticsearch/plugin/elasticsearch-river-jdbc/1.4.0.10/elasticsearch-river-jdbc-1.4.0.10.zip`. See the [River JDBC Plugin Installation Instructions](https://github.com/jprante/elasticsearch-river-jdbc#installation) for complete information.
+2. We also need to grab [SQLite JDBC Driver](https://bitbucket.org/xerial/sqlite-jdbc/downloads) `version 3.7.2` and `version 3.8.7`, and save it to `$ES_HOME/plugin/jdbc`. See Troubleshooting for more information
+
+#### Getting your sms.db
+##### OS X
+1. Open `~/Library/Application Support/MobileSync/Backup`
+2. Go into the folder corresponding to your device (if you just did a backup look for the newest folder)
+3. Find the file named `3d0d7e5fb2ce288813306e4d4636395e047a3d28`
+4. Copy the file somewhere, and rename it to `sms.db`
+
+##### Windows
+1. Navigate to `%APPDATA%\Apple Computer\MobileSync\Backup\`
+2. Go into the folder corresponding to your device 
+3. Find the file named `3d0d7e5fb2ce288813306e4d4636395e047a3d28`
+4. Copy the file somewhere, and rename it to `sms.db`
+
+
+## Load messages into Elasticsearch
+1. Start elasticsearch by running `$ES_HOME/bin/elasticsearch`
+2. Change the path of `String smsDb = "sms.db"` in ElasticSMS.java to point towards your sms.db file
+3. Run ElasticSMS to load all the data into Elasticsearch
+
+## Browse the data with Kibana
+1. Download [Kibana](https://www.elastic.co/downloads/kibana)
+2. Start kibana by running `$KA_HOME/bin/kibana`
+3. When asked for an index name enter `message*` and choose `date` as Time-field name
+4. Explore your messages!
+
+## Troubleshooting
+* There is currently an issue with the `SQLite JDBC driver 3.8.7` (see [setReadOnly is not supported by SQLite](https://github.com/jprante/elasticsearch-river-jdbc/issues/250)) and elasticsearch river, but by adding both version 3.7.2 and 3.8.7 to the `$ES_HOME/plugin/jdbc` directory it seems to be working properly.
+* On OS X there is some issue with SQLite and river jdbc plugin causing a
+`Caused by: java.sql.SQLException: [SQLITE_NOTADB]  File opened that is not a database file (file is encrypted or is not a database)` even though it is pointed towards the correct database file
---
+++ src/main/java/com/thord/ElasticSMS.java
@@ -0,0 +1,146 @@
+package com.thord;
+
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+/**
+ * Simple class that loads all messages from a given sms.db into Elasticsearch
+ * @author thordy
+ */
+public class ElasticSMS {
+	private static final String LOAD_MESSAGES_SQL = "SELECT"
+			+ "   m.ROWID AS '_id',"
+			+ "   m.guid,"
+			+ "   m.text,"
+			+ "   h.id AS 'from',"
+			+ "   m.guid,"
+			+ "   m.text,"
+			+ "   m.replace,"
+			+ "   m.service_center,"
+			+ "   m.handle_id,"
+			+ "   m.subject,"
+			+ "   m.country,"
+			+ "   m.version,"
+			+ "   m.type,"
+			+ "   m.service,"
+			+ "   m.account,"
+			+ "   m.account_guid,"
+			+ "   m.error,"
+			+ "   m.is_delivered,"
+			+ "   m.is_finished,"
+			+ "   m.is_emote,"
+			+ "   m.is_from_me,"
+			+ "   m.is_empty,"
+			+ "   m.is_delayed,"
+			+ "   m.is_auto_reply,"
+			+ "   m.is_prepared,"
+			+ "   m.is_read,"
+			+ "   m.is_system_message,"
+			+ "   m.is_forward,"
+			+ "   m.was_downgraded,"
+			+ "   m.is_archive,"
+			+ "   m.cache_has_attachments,"
+			+ "   m.cache_roomnames,"
+			+ "   m.was_data_detected,"
+			+ "   m.was_deduplicated,"
+			+ "   m.is_audio_message,"
+			+ "   m.is_played,"
+			+ "   m.date_played,"
+			+ "   m.item_type,"
+			+ "   m.other_handle,"
+			+ "   m.group_title,"
+			+ "   m.group_action_type,"
+			+ "   m.share_status,"
+			+ "   m.share_direction,"
+			+ "   m.is_expirable,"
+			+ "   m.expire_state,"
+			+ "   m.message_action_type,"
+			+ "   m.message_source,"
+			+ "   DATETIME(m.date + strftime('%s', '2001-01-01 00:00:00'), 'unixepoch', 'localtime') AS 'date',"
+			+ "   DATETIME(m.date_read + strftime('%s', '2001-01-01 00:00:00'), 'unixepoch', 'localtime') AS 'date_read',"
+			+ "   DATETIME(m.date_delivered + strftime('%s', '2001-01-01 00:00:00'), 'unixepoch', 'localtime') AS 'date_delivered'"
+			+ " FROM message m, handle h"
+			+ " WHERE"
+			+ "   m.handle_id = h.ROWID";
+
+	public static void main(String[] args) throws IOException {
+		// Path to the sms.db file. This can also be a full path,
+		// either '/full/path/to/file/sms.db' on OSX or 'C:\\\\files\\\\sms.db' on windows. Note
+		// that you need to use four backslashes to escape things properly.
+		String smsDb = "sms.db";
+		smsDb = "/Users/thord/Dropbox/usr/backup/sms2.db";
+
+		String elasticHost = "localhost";
+		int elasticPort = 9200;
+
+		String elasticIndex = "messages";
+		String type = "message";
+
+		String mapping = ""
+				+ "\"" + type + "\" : {"
+				+ "	\"properties\" : {"
+				+ "		\"date\" : {"
+				+ "			\"type\" : \"date\","
+				+ "			\"index\" : \"not_analyzed\","
+				+ "			\"format\" : \"YYYY-MM-dd HH:mm:ss\""
+				+ "		},"
+				+ "		\"date_delivered\" : {"
+				+ "			\"type\" : \"date\","
+				+ "			\"index\" : \"not_analyzed\","
+				+ "			\"format\" : \"YYYY-MM-dd HH:mm:ss\""
+				+ "		},"
+				+ "		\"date_played\" : {"
+				+ "			\"type\" : \"date\","
+				+ "			\"index\" : \"not_analyzed\","
+				+ "			\"format\" : \"YYYY-MM-dd HH:mm:ss\""
+				+ "		},"
+				+ "		\"date_read\" : {"
+				+ "			\"type\" : \"date\","
+				+ "			\"index\" : \"not_analyzed\","
+				+ "			\"format\" : \"YYYY-MM-dd HH:mm:ss\""
+				+ "		},"
+				+ "		\"from\" : {"
+				+ "			\"type\" : \"string\","
+				+ "			\"index\" : \"not_analyzed\""
+				+ "		},"
+				+ "		\"guid\" : {"
+				+ "			\"type\" : \"string\","
+				+ "			\"index\" : \"not_analyzed\""
+				+ "		},"
+				+ "		\"_id\" : {"
+				+ "			\"type\" : \"integer\""
+				+ "		}"
+				+ "	}"
+				+ "}";
+
+		String jsonString = ""
+				+ "{" +
+				"	\"type\" : \"jdbc\"," +
+				"	\"jdbc\" : {" + 
+				"		\"url\" : \"jdbc:sqlite:" + smsDb + "\"," +
+				"		\"user\" : \"\"," +
+				"		\"password\" : \"\"," +
+				"		\"sql\" : \"" + LOAD_MESSAGES_SQL + "\"," +
+				"		\"index\" : \"" + elasticIndex + "\"," +
+				"		\"type\" : \"" + type + "\"," +
+				"		\"type_mapping\" : {" + mapping + "}" +
+				"	}" +
+				"}";
+		StringEntity entity = new StringEntity(jsonString);
+
+		HttpClient httpClient = HttpClientBuilder.create().build();
+
+		String url = "http://" + elasticHost  + ":" + elasticPort + "/_river/" + elasticIndex + "/_meta";
+		HttpPut putRequest = new HttpPut(url);
+
+		putRequest.setEntity(entity);
+		HttpResponse response = httpClient.execute(putRequest);
+
+		System.out.println(response);
+	}
+}
